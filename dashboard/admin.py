6aee87c32b226c052e0bf4495b0205b461b0d848
ikepawsat <ike.pawsat@gmail.com>
2025-11-12 15:03:10 -0500

    maybe this will work!

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,16 +1,3 @@
 from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
-from .models import BostonQualifier
-
-class BostonQualifierResource(resources.ModelResource):
-    class Meta:
-        model = BostonQualifier
-        import_id_fields = ["resultId"]
-        skip_unchanged = True
-        report_skipped = True
-
-@admin.register(BostonQualifier)
-class BostonQualifierAdmin(ImportExportModelAdmin):
-    resource_class = BostonQualifierResource
-    pass
